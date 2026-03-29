ad3deb5d1219f46e82e30ea7a57d8b123e3eeefc
vtplic <mrxman.mt@gmail.com>
2026-03-30 00:44:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title="KPI Dashboard", layout="wide")
 
-st.markdown("<h1 style='text-align: center;'>📊 KPI Dashboard</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>📊 KPI Dashboard Bưu cục Liên Chiểu</h1>", unsafe_allow_html=True)
 
 # ===== INIT KPI =====
 default_kpi = {
